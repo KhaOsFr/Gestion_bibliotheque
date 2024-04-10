529741dacadf31a3ae8306cfc6d20f9ab3c000e8
simon.robert <simrob59127@live.fr>
2024-04-10 12:26:45 +0200

    Début énoncé v3

---
+++ src/bibliotheque/mvc/view/AbstractViewExemplaire.java
@@ -0,0 +1,29 @@
+package bibliotheque.mvc.view;
+
+import bibliotheque.metier.Exemplaire;
+import bibliotheque.mvc.observer.Observer;
+
+import java.util.List;
+
+public abstract class AbstractViewExemplaire implements Observer {
+    protected ExemplaireController exemplaireController;
+    protected List<Exemplaire> le;
+
+    public void setController(ExemplaireController exemplaireController) {
+        this.exemplaireController = exemplaireController;
+    }
+
+    public abstract void menu();
+
+    public abstract void affList(List le);
+
+    public List<Exemplaire> getAll(){
+        return le;
+    }
+
+    @Override
+    public void update(List le) {
+        this.le = le;
+        affList(le);
+    }
+}
